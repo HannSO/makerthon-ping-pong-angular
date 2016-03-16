65c49397fe4aa7f5ab8a56e2b41711f4d694d7fc
ZeshanRasul <zeshanrasul90@gmail.com>
2016-03-16 18:09:47 +0000

    Updated submit results controller

--- js/submitResultsController.js
+++ js/submitResultsController.js
@@ -10,13 +10,13 @@ var makeGamePath = ('http://localhost:3000/games/new');
 
   self.submitResults = function(){
 
-    $http.post(makeGamePath, JSON.stringify({winner:self.winner,loser:self.loser}))
+    $http.post(makeGamePath, {winner:self.winner,loser:self.loser});
 
-    .success(function(){
+    // .success(function(){
       // UPDATE LEADERBOARD METHOD
       // self.confirmation = true;
 
-    });
+    // });
     //
     // .error(function(){
     //   self.unconfirmation = true;
